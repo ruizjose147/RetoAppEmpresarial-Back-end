b52fb21646212550578d0980b0aae0968f793979
ruizjose147 <ruizjose_147@hotmail.com>
2022-03-31 19:14:16 -0500

    Limpiando el codigo

--- FerreteriaDonRaul/src/main/java/co/com/sofka/app/FerreteriaDonRaul/repositories/ProveedorRepositorieI.java
+++ FerreteriaDonRaul/src/main/java/co/com/sofka/app/FerreteriaDonRaul/repositories/ProveedorRepositorieI.java
@@ -1,6 +1,5 @@
 package co.com.sofka.app.FerreteriaDonRaul.repositories;
 
-import co.com.sofka.app.FerreteriaDonRaul.models.Cliente;
 import co.com.sofka.app.FerreteriaDonRaul.models.Proveedor;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
--- FerreteriaDonRaul/src/main/java/co/com/sofka/app/FerreteriaDonRaul/repositories/VendedorRepositorieI.java
+++ FerreteriaDonRaul/src/main/java/co/com/sofka/app/FerreteriaDonRaul/repositories/VendedorRepositorieI.java
@@ -1,6 +1,5 @@
 package co.com.sofka.app.FerreteriaDonRaul.repositories;
 
-import co.com.sofka.app.FerreteriaDonRaul.models.Cliente;
 import co.com.sofka.app.FerreteriaDonRaul.models.Vendedor;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
